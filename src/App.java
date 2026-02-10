e71c502b24d9115cfb96a1b4baf91f55115abfc2
HaiseSasaki1298 <joaquinjose1298@gmail.com>
2026-02-10 23:39:27 +0000

    Mensaje de que  tal te va la tarde

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esto es una  prueba 3");
+        System.out.println("Hola juanjo que tal te va tarde");
     }
 }
