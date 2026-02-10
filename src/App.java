4df0c247ca2c9978385337a253b8e126de1dac8f
Juanjo <veltrox2002@gmail.com>
2026-02-10 23:40:56 +0000

    Delvolver las grancias

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola juanjo que tal te va tarde");
+                System.out.println("Perfectamente  joaquin gracias por preguntar");
+
     }
 }
